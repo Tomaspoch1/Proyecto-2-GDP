d48e57c5133f7d5cece990468f081624ea4622db
Tomaspoch1 <tpoch@miuandes.cl>
2024-09-11 00:01:49 -0300

    Texto centrado

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, True, color)
-    textrect = textobj.get_rect()
-    textrect.topleft = (x, y)
+    textrect = textobj.get_rect(center=(x, y))
     surface.blit(textobj, textrect)
