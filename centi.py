22e16287f443cbd180354ca2e31074d1ef907aa8
Emtyloc <mitoro1@miuandes.cl>
2024-09-11 23:35:29 -0300

    feat: centipede implementation.

---
+++ centi.py
@@ -0,0 +1,80 @@
+import pygame, random
+
+
+class Centi(pygame.sprite.Sprite):
+    gif_delay = 0
+    gif_counter = 0
+    gif = []
+
+    def __init__(self, x, y, SCREEN_WIDTH, SCREEN_HEIGHT, SPEED):
+        pygame.sprite.Sprite.__init__(self)
+        self.img_size = (20, 20)
+        self.image = pygame.image.load("centipede1.png")
+        self.gif.append(pygame.image.load("centipede1.png"))
+        self.gif.append(pygame.image.load("centipede2.png"))
+        transparency_color = self.image.get_at((1, 1))
+        self.image.set_colorkey(transparency_color)
+        for i in range(2):
+            self.gif[i].set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.left_right = 1
+        self.up_down = 1
+        self.vertical_move = 0
+        self.SCREEN_WIDTH = SCREEN_WIDTH
+        self.SCREEN_HEIGHT = SCREEN_HEIGHT
+        self.SPEED = SPEED
+
+    def update(self):
+        # Gif
+        self.gif_delay += 1
+        if self.gif_delay % 4 == 0:
+            self.image = self.gif[self.gif_counter]
+            self.gif_counter += 1
+            self.gif_delay = 0
+            if self.gif_counter == 2:
+                self.gif_counter = 0
+        # left to right
+        if self.vertical_move > 0:
+            self.vertical_move -= 1
+
+        if self.left_right == 1 and self.vertical_move == 0:
+            self.rect.x += self.SPEED
+            # right wall
+            if self.rect.x > self.SCREEN_WIDTH:
+                self.rect.x -= self.SPEED
+                self.collide()
+                self.vertical_move -= 1
+                # last row
+                if self.rect.y >= self.SCREEN_HEIGHT:
+                    self.up_down = 0
+                    self.rect.x -= self.SPEED
+        elif self.left_right == 0 and self.vertical_move == 0:
+            self.rect.x -= self.SPEED
+            # left wall
+            if self.rect.x < 0:
+                self.rect.x += self.SPEED
+                self.collide()
+                self.vertical_move -= 1
+                # last row
+                if self.rect.y >= self.SCREEN_HEIGHT:
+                    self.up_down = 0
+
+    def collide(self):
+        self.vertical_move = 2
+        if self.left_right == 1:
+            self.left_right = 0
+            if self.up_down == 1:
+                self.rect.y += 20
+            else:
+                self.rect.y -= 20
+        elif self.left_right == 0:
+            self.left_right = 1
+            if self.up_down == 1:
+                self.rect.y += 20
+            else:
+                self.rect.y -= 20
+
+    def __str__(self) -> str:
+        return f"X: {self.rect.x} Y: {self.rect.y}"
--- game.py
+++ game.py
@@ -2,8 +2,9 @@ import pygame
 from config import SCREEN_WIDTH, SCREEN_HEIGHT, BLACK, WHITE, RED, GREEN
 from player import Player
 from bullet import BulletManager
-from centipede import Centipede
+from centi import Centi  # Asegúrate de que está importando la nueva clase Centi
 from utils import draw_text
+
 
 class Game:
     def __init__(self, screen):
@@ -12,22 +13,31 @@ class Game:
         self.screen_height = SCREEN_HEIGHT
         self.font = pygame.font.SysFont(None, 55)
         self.game_over = False
-        
+
         # Inicializa el jugador
-        self.player = Player(self.screen_width // 2, self.screen_height - 50, 30, 5, self.screen_width)
-        
+        self.player = Player(
+            self.screen_width // 2, self.screen_height - 50, 30, 5, self.screen_width
+        )
+
         # Inicializa el administrador de balas
         self.bullet_manager = BulletManager(bullet_delay=500)
-        
-        # Inicializa el centipede
-        self.centipede = Centipede(10, 20, 3, self.screen_width)
+
+        # Inicializa el grupo de ciempiés
+        self.centipedes = pygame.sprite.Group()
+        self.CENTIS_NUMBERS = 12
+        CENTI_SPEED = 5
+        for m in range(self.CENTIS_NUMBERS):
+            centi = Centi(
+                20 * m, -20, self.screen_width, self.screen_height, CENTI_SPEED
+            )
+            self.centipedes.add(centi)
 
     def run(self):
         if not self.game_over:
             self.screen.fill(BLACK)
             self.handle_events()
             self.bullet_manager.update(self.screen_height)
-            self.centipede.move()
+            self.centipedes.update()
             self.check_collisions()
             self.draw_objects()
         else:
@@ -37,25 +47,32 @@ class Game:
         keys = pygame.key.get_pressed()
         self.player.move(keys)
         if keys[pygame.K_SPACE]:
-            self.bullet_manager.shoot(self.player.rect.centerx - 2.5, self.player.rect.top)
+            self.bullet_manager.shoot(
+                self.player.rect.centerx - 2.5, self.player.rect.top
+            )
         if keys[pygame.K_ESCAPE]:
             self.game_over = True
 
     def check_collisions(self):
         for bullet in self.bullet_manager.bullets:
-            for segment in self.centipede.segments:
-                if bullet.rect.colliderect(segment):
-                    self.bullet_manager.bullets.remove(bullet)
-                    self.centipede.segments.remove(segment)
-                    break
-        if not self.centipede.segments:
-            self.game_over = True  # Termina el juego si el centipede está vacío
+            hit_list = pygame.sprite.spritecollide(bullet, self.centipedes, True)
+            if hit_list:
+                self.bullet_manager.bullets.remove(bullet)
+                if len(self.centipedes) == 0:
+                    self.game_over = True
 
     def draw_objects(self):
         self.player.draw(self.screen, GREEN)
         self.bullet_manager.draw(self.screen, WHITE)
-        self.centipede.draw(self.screen, RED)
+        self.centipedes.draw(self.screen)
 
     def show_game_over(self):
         self.screen.fill(BLACK)
-        draw_text('¡Juego Terminado!', self.font, RED, self.screen, self.screen_width // 2, self.screen_height // 2)
+        draw_text(
+            "¡Juego Terminado!",
+            self.font,
+            RED,
+            self.screen,
+            self.screen_width // 2,
+            self.screen_height // 2,
+        )
