cdf1351977e3deaef5fae24dccdeda166dc3262a
Tomaspoch1 <tpoch@miuandes.cl>
2024-09-25 20:13:44 -0300

    prueba ejecutable 2

--- src/game/centi.py
+++ src/game/centi.py
@@ -1,4 +1,16 @@
 import pygame
+import os
+import sys
+
+# Configuración de las rutas de los recursos
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Rutas de las imágenes
+image1_path = os.path.join(base_path, "assets", "centipede1.png")
+image2_path = os.path.join(base_path, "assets", "centipede2.png")
 
 
 class Centi(pygame.sprite.Sprite):
@@ -7,15 +19,20 @@ class Centi(pygame.sprite.Sprite):
     gif = []
 
     def __init__(self, x, y, SCREEN_WIDTH, SCREEN_HEIGHT, SPEED):
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__()  # Inicializa el sprite
         self.img_size = (20, 20)
-        self.image = pygame.image.load("assets/centipede1.png")
-        self.gif.append(pygame.image.load("assets/centipede1.png"))
-        self.gif.append(pygame.image.load("assets/centipede2.png"))
+
+        # Carga las imágenes usando las rutas definidas
+        self.image = pygame.image.load(image1_path)
+        self.gif.append(pygame.image.load(image1_path))
+        self.gif.append(pygame.image.load(image2_path))
+
+        # Configura la transparencia de las imágenes
         transparency_color = self.image.get_at((1, 1))
         self.image.set_colorkey(transparency_color)
         for i in range(2):
             self.gif[i].set_colorkey((0, 0, 0))
+
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -35,29 +52,28 @@ class Centi(pygame.sprite.Sprite):
             self.gif_delay = 0
             if self.gif_counter == 2:
                 self.gif_counter = 0
-        # left to right
+        
+        # Lógica de movimiento
         if self.vertical_move > 0:
             self.vertical_move -= 1
 
         if self.left_right == 1 and self.vertical_move == 0:
             self.rect.x += self.SPEED
-            # right wall
+            # Verificación de colisión con la pared derecha
             if self.rect.x > self.SCREEN_WIDTH:
                 self.rect.x -= self.SPEED
                 self.collide()
                 self.vertical_move -= 1
-                # last row
                 if self.rect.y >= self.SCREEN_HEIGHT:
                     self.up_down = 0
                     self.rect.x -= self.SPEED
         elif self.left_right == 0 and self.vertical_move == 0:
             self.rect.x -= self.SPEED
-            # left wall
+            # Verificación de colisión con la pared izquierda
             if self.rect.x < 0:
                 self.rect.x += self.SPEED
                 self.collide()
                 self.vertical_move -= 1
-                # last row
                 if self.rect.y >= self.SCREEN_HEIGHT:
                     self.up_down = 0
 
